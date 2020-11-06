92bf2f1cccdf5b1b003893a72ee4ff23f7437c65
Emil Krastev <Emil.Krastev@paysafe.com>
2020-11-06 17:25:42 +0200

    Second commit

--- src/main/java/com/example/relaxedbinding/Configuration.java
+++ src/main/java/com/example/relaxedbinding/Configuration.java
@@ -11,27 +11,6 @@ import java.net.URISyntaxException;
 @Validated
 public class Configuration {
 
-
-  private static final String IS_NOT_ONLY_NUMBERS = "(?![0-9]+$)";
-  private static final String DOES_NOT_START_WITH_DASH = "(?<!-)";
-  private static final String DOES_NOT_END_WITH_DASH = "(?!-)";
-  private static final String CONTAINS_NO_MORE_THAN_63_ALPHANUMERICAL_CHARS = "[a-zA-Z0-9-.]{0,63}";
-
-  /**
-   * The hostname where the vault resides.
-   */
-  private String host;
-
-  /**
-   * The port on which the vault should be accessed.
-   */
-  private Integer port = 8200;
-
-  /**
-   * The scheme to be used. Either 'http' or 'https'.
-   */
-  private String scheme = "https";
-
   private Token token;
 
   public String getTokenRuntime() {
@@ -46,43 +25,8 @@ public class Configuration {
     this.token = token;
   }
 
-  public String getHost() {
-    return host;
-  }
-
-  public void setHost(String host) {
-    this.host = host;
-  }
-
-  public Integer getPort() {
-    return port;
-  }
-
-  public void setPort(Integer port) {
-    this.port = port;
-  }
-
-  public String getScheme() {
-    return scheme;
-  }
-
-  public void setScheme(String scheme) {
-    this.scheme = scheme;
-  }
-
-  public URI getUri() {
-    try {
-      return new URI(scheme, null, host, port, null, null, null);
-    } catch (URISyntaxException uriSyntaxException) {
-      throw new IllegalArgumentException("Vault credentials cannot build up a valid URL.", uriSyntaxException);
-    }
-  }
-
   public static class Token {
 
-    /**
-     * The token to be used when accessing vault runtime.
-     */
     private String runtime;
 
     public String getRuntime() {
